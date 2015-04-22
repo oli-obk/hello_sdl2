d25c42c6e1e07b85d4c1103625fd8cbb9f75f102
Alexander Rødseth <rodseth@gmail.com>
2015-04-22 14:40:27 +0200

    Moving samples that does build

--- fpc/checkout.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm -rf SDL2
-git clone https://github.com/danpla/sdl2-fpc SDL2
-mv SDL2/SDL2 temp
-rm -rf SDL2
-mv temp SDL2
-chmod -x SDL2/*
--- lua/README.md
+++
@@ -1,6 +0,0 @@
-Instructions
-------------
-
-Install sdl2 for Lua with luarocks:
-
-`sudo luarocks install lua-sdl2`
